c5f881bf988a30e6f70d5592e40c5ea13ddbc6c2
JustHumanz <Aldinsetiawan99@Gmail.com>
2019-06-30 02:22:20 +0700

    Update Sql

--- Dump20190630.sql
+++ Dump20190630.sql
@@ -37,7 +37,7 @@ CREATE TABLE `admin` (
 
 LOCK TABLES `admin` WRITE;
 /*!40000 ALTER TABLE `admin` DISABLE KEYS */;
-INSERT INTO `admin` VALUES (1,'Aldin Setiawan','admin','1sampai8');
+INSERT INTO `admin` VALUES (1,'Aldin Setiawan','admin','1sampai8'),(2,'Irfan','user','1');
 /*!40000 ALTER TABLE `admin` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -61,7 +61,7 @@ CREATE TABLE `advance` (
 
 LOCK TABLES `advance` WRITE;
 /*!40000 ALTER TABLE `advance` DISABLE KEYS */;
-INSERT INTO `advance` VALUES (1,'Xubuntu'),(2,'Lubuntu'),(3,'Ubuntu-Studio'),(4,'Debian'),(5,'Kali'),(6,'Arch'),(7,'Manjaro'),(8,'Fedora'),(9,'Parrot'),(10,'Tails'),(11,'OpenSUSE'),(12,'Android-x86'),(13,'Solus'),(14,'CentOs');
+INSERT INTO `advance` VALUES (1,'Xubuntu'),(2,'Lubuntu'),(3,'Ubuntu-Studio'),(4,'Debian'),(5,'Kali'),(6,'Arch'),(7,'Manjaro'),(8,'Fedora'),(9,'Parrot'),(10,'Tails'),(11,'OpenSUSE'),(12,'Android-x86'),(13,'Solus'),(14,'CentOs'),(15,'Tails');
 /*!40000 ALTER TABLE `advance` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -85,7 +85,7 @@ CREATE TABLE `beginner` (
 
 LOCK TABLES `beginner` WRITE;
 /*!40000 ALTER TABLE `beginner` DISABLE KEYS */;
-INSERT INTO `beginner` VALUES (1,'Ubuntu'),(2,'Kubuntu'),(3,'Ubuntu-Mate'),(4,'Ubuntu-Budgie'),(5,'Deepin'),(6,'Zorin'),(7,'Mint'),(8,'MX'),(9,'Elementary'),(10,'Lubuntu'),(11,'Xubuntu'),(12,'Debian'),(13,'Manjaro'),(14,'Pop-OS!'),(15,'OpenSUSE'),(16,'Endless-OS'),(17,'Android-x86'),(18,'Solus'),(19,'BlankOn');
+INSERT INTO `beginner` VALUES (1,'Ubuntu'),(2,'Kubuntu'),(3,'Ubuntu-Mate'),(4,'Ubuntu-Budgie'),(5,'Deepin'),(6,'Zorin'),(7,'Mint'),(8,'MX'),(9,'Elementary'),(10,'Lubuntu'),(11,'Xubuntu'),(12,'Debian'),(13,'Manjaro'),(14,'Pop-OS!'),(15,'OpenSUSE'),(16,'Endless-OS'),(17,'Android-x86'),(18,'Solus'),(19,'BlankOn'),(20,'Ubuntu-Studio'),(21,'Tails'),(22,'Pop!-OS');
 /*!40000 ALTER TABLE `beginner` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -157,7 +157,7 @@ CREATE TABLE `friendly` (
 
 LOCK TABLES `friendly` WRITE;
 /*!40000 ALTER TABLE `friendly` DISABLE KEYS */;
-INSERT INTO `friendly` VALUES (1,'Ubuntu'),(2,'Xubuntu'),(3,'Kubuntu'),(4,'Ubuntu-Mate'),(5,'Ubuntu-Budgie'),(6,'Deepin'),(7,'Zorin'),(8,'Mint'),(9,'Manjaro'),(10,'MX'),(11,'Elementary'),(12,'Tails'),(13,'Pop!-OS'),(14,'OpenSUSE'),(15,'Endless-OS'),(16,'Android-x86'),(17,'Backbox'),(18,'Solus'),(19,'BlankOn'),(20,'CentOS'),(21,'Ubuntu-Studio'),(22,'Debian');
+INSERT INTO `friendly` VALUES (1,'Ubuntu'),(2,'Xubuntu'),(3,'Kubuntu'),(4,'Ubuntu-Mate'),(5,'Ubuntu-Budgie'),(6,'Deepin'),(7,'Zorin'),(8,'Mint'),(9,'Manjaro'),(10,'MX'),(11,'Elementary'),(12,'Tails'),(13,'Pop!-OS'),(14,'OpenSUSE'),(15,'Endless-OS'),(16,'Android-x86'),(17,'BackBox'),(18,'Solus'),(19,'BlankOn'),(20,'CentOS'),(21,'Ubuntu-Studio'),(22,'Debian');
 /*!40000 ALTER TABLE `friendly` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -181,7 +181,7 @@ CREATE TABLE `high` (
 
 LOCK TABLES `high` WRITE;
 /*!40000 ALTER TABLE `high` DISABLE KEYS */;
-INSERT INTO `high` VALUES (1,'Ubuntu'),(2,'Ubuntu-Studio'),(3,'Deepin'),(4,'Kali'),(5,'Fedora'),(6,'Pop!-OS'),(7,'Endless-OS'),(8,'CentOS'),(9,'Ubuntu-Mate'),(10,'Ubuntu-Budgie'),(11,'Debian'),(12,'Arch'),(13,'Tails'),(14,'OpenSUSE');
+INSERT INTO `high` VALUES (1,'Ubuntu'),(2,'Ubuntu-Studio'),(3,'Deepin'),(4,'Kali'),(5,'Fedora'),(6,'Pop!-OS'),(7,'Endless-OS'),(8,'CentOS'),(9,'Ubuntu-Mate'),(10,'Ubuntu-Budgie'),(11,'Debian'),(12,'Arch'),(13,'Tails'),(14,'OpenSUSE'),(15,'Ubuntu-Studio');
 /*!40000 ALTER TABLE `high` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -234,7 +234,7 @@ CREATE TABLE `main` (
 
 LOCK TABLES `main` WRITE;
 /*!40000 ALTER TABLE `main` DISABLE KEYS */;
-INSERT INTO `main` VALUES (1,'Ubuntu','Active','Debian-Like/Debian-Base','Apt','19.04 Disco Dingo','https://ubuntu.com/download/desktop'),(2,'Xubuntu','Active','Debian-Like/Debian-Base','Apt','19.04 Disco Dingo','https://xubuntu.org/download'),(3,'Lubuntu','Active','Debian-Like/Debian-Base','Apt','19.04 Disco Dingo','https://lubuntu.net/downloads'),(4,'Kubuntu','Active','Debian-Like/Debian-Base','Apt','19.04 Disco Dingo','https://kubuntu.org/getkubuntu/'),(5,'Ubuntu-Mate','Active','Debian-Like/Debian-Base','Apt','19.04 Disco Dingo','https://ubuntu-mate.org/download/'),(6,'Ubuntu-Budgie','Active','Debian-Like/Debian-Base','Apt','19.04 Disco Dingo','https://ubuntubudgie.org/downloads'),(7,'Ubuntu-Studio','Active','Debian-Like/Debian-Base','Apt','19.04 Disco Dingo','https://ubuntustudio.org/download/'),(8,'Debian','Active','Debian-Like/Debian-Base','Apt','9 Stretch','https://www.debian.org/CD/http-ftp/'),(9,'Deepin','Active','Debian-Like/Debian-Base','Apt','15.1','https://www.deepin.org/pb/download/'),(10,'Kali','Active','Debian-Like/Debian-Base','Apt','Rolling Release','https://www.kali.org/downloads/'),(11,'Zorin','Active','Debian-Like/Debian-Base','Apt','15','https://zorinos.com/download/'),(12,'Mint','Active','Debian-Like/Debian-Base','Apt','19.1 Tessa','https://linuxmint.com/download.php'),(13,'Arch','Active','Independent/Arch-Base','Pacman','Rolling Release','https://www.archlinux.org/download/'),(14,'Manjaro','Active','Arch-Like/Arch-Base','Pacman','Rolling Release','https://manjaro.org/download/'),(15,'MX','Active','Debian-Like/Debian-Base','Apt','MX-18.3','https://mxlinux.org/download-links#Mirrors'),(16,'Elementary','Active','Debian-Like/Debian-Base','Apt','5.0 Juno','https://elementary.io/'),(17,'Fedora','Active','Independent/Rhel-Base','Dnf','30','https://getfedora.org/en/workstation/download/'),(18,'Parrot','Active','Debian-Like/Debian-Base','Apt','Rolling Release','https://www.parrotsec.org/download.php'),(19,'Gentoo','Active','Independent','Portage','Rolling Release','http://www.gentoo.org/main/en/mirrors.xml'),(20,'LFS','Active','Independent','-','Rolling Release','http://www.linuxfromscratch.org/lfs/download.html'),(21,'Tails','Active','Debian-Like/Debian-Base','Apt','8.3','https://tails.boum.org/download'),(22,'Pop!-OS','Active','Debian-Like/Debian-Base','Apt','19.04','https://system76.com/pop'),(23,'openSUSE','Active','Independent','Zypper','15.1 LEAP','http://software.opensuse.org/'),(24,'Endless-OS','Active','Debian-Like/Debian-Base','Apt','3.6.0','https://endlessm.com/download/'),(25,'Android-x86','Active','Android-Like/Android-Base','-','8.1 Oreo','http://www.android-x86.org/download'),(26,'BackBox','Active','Debian-Like/Debian-Base','Apt','5','http://www.backbox.org/downloads'),(27,'Solus','Active','Independent','Eopkg','4','https://getsol.us/download/'),(28,'BlankOn','Active','Debian-Like/Debian-Base','Apt','XI ULUWATU','https://www.blankonlinux.or.id/#UNDUH'),(29,'DracOs','Active','Independent/LFS-Base','Pacman','4.0 Santet','https://dracos-linux.org/downloads.php'),(30,'CentOS','Active','Independent/Rhel-Base','Yum','7','http://www.centos.org/download/');
+INSERT INTO `main` VALUES (1,'Ubuntu','Active','Debian-Like/Debian-Base','Apt','19.04 Disco Dingo','https://ubuntu.com/download/desktop'),(2,'Xubuntu','Active','Debian-Like/Debian-Base','Apt','19.04 Disco Dingo','https://xubuntu.org/download'),(3,'Lubuntu','Active','Debian-Like/Debian-Base','Apt','19.04 Disco Dingo','https://lubuntu.net/downloads'),(4,'Kubuntu','Active','Debian-Like/Debian-Base','Apt','19.04 Disco Dingo','https://kubuntu.org/getkubuntu/'),(5,'Ubuntu-Mate','Active','Debian-Like/Debian-Base','Apt','19.04 Disco Dingo','https://ubuntu-mate.org/download/'),(6,'Ubuntu-Budgie','Active','Debian-Like/Debian-Base','Apt','19.04 Disco Dingo','https://ubuntubudgie.org/downloads'),(7,'Ubuntu-Studio','Active','Debian-Like/Debian-Base','Apt','19.04 Disco Dingo','https://ubuntustudio.org/download/'),(8,'Debian','Active','Debian-Like/Debian-Base','Apt','9 Stretch','https://www.debian.org/CD/http-ftp/'),(9,'Deepin','Active','Debian-Like/Debian-Base','Apt','15.1','https://www.deepin.org/pb/download/'),(10,'Kali','Active','Debian-Like/Debian-Base','Apt','Rolling Release','https://www.kali.org/downloads/'),(11,'Zorin','Active','Debian-Like/Debian-Base','Apt','15','https://zorinos.com/download/'),(12,'Mint','Active','Debian-Like/Debian-Base','Apt','19.1 Tessa','https://linuxmint.com/download.php'),(13,'Arch','Active','Independent/Arch-Base','Pacman','Rolling Release','https://www.archlinux.org/download/'),(14,'Manjaro','Active','Arch-Like/Arch-Base','Pacman','Rolling Release','https://manjaro.org/download/'),(15,'MX','Active','Debian-Like/Debian-Base','Apt','MX-18.3','https://mxlinux.org/download-links#Mirrors'),(16,'Elementary','Active','Debian-Like/Debian-Base','Apt','5.0 Juno','https://elementary.io/'),(17,'Fedora','Active','Independent/Rhel-Base','Dnf','30','https://getfedora.org/en/workstation/download/'),(18,'Parrot','Active','Debian-Like/Debian-Base','Apt','Rolling Release','https://www.parrotsec.org/download.php'),(19,'Gentoo','Active','Independent','Portage','Rolling Release','http://www.gentoo.org/main/en/mirrors.xml'),(20,'LFS','Active','Independent','-','Rolling Release','http://www.linuxfromscratch.org/lfs/download.html'),(21,'Tails','Active','Debian-Like/Debian-Base','Apt','8.3','https://tails.boum.org/download'),(22,'Pop!-OS','Active','Debian-Like/Debian-Base','Apt','19.04','https://system76.com/pop'),(23,'OpenSUSE','Active','Independent','Zypper','15.1 LEAP','http://software.opensuse.org/'),(24,'Endless-OS','Active','Debian-Like/Debian-Base','Apt','3.6.0','https://endlessm.com/download/'),(25,'Android-x86','Active','Android-Like/Android-Base','-','8.1 Oreo','http://www.android-x86.org/download'),(26,'BackBox','Active','Debian-Like/Debian-Base','Apt','5','http://www.backbox.org/downloads'),(27,'Solus','Active','Independent','Eopkg','4','https://getsol.us/download/'),(28,'BlankOn','Active','Debian-Like/Debian-Base','Apt','XI ULUWATU','https://www.blankonlinux.or.id/#UNDUH'),(29,'DracOs','Active','Independent/LFS-Base','Pacman','4.0 Santet','https://dracos-linux.org/downloads.php'),(30,'CentOS','Active','Independent/Rhel-Base','Yum','7','http://www.centos.org/download/');
 /*!40000 ALTER TABLE `main` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -306,7 +306,7 @@ CREATE TABLE `secure` (
 
 LOCK TABLES `secure` WRITE;
 /*!40000 ALTER TABLE `secure` DISABLE KEYS */;
-INSERT INTO `secure` VALUES (1,'Deepin'),(2,'Kali'),(3,'Mint'),(4,'Arch'),(5,'Fedora'),(6,'Parrot'),(7,'Gentoo'),(8,'Tails'),(9,'OpenSUSE'),(10,'Android-x86'),(11,'Backbox'),(12,'Solus'),(13,'DracOS'),(14,'CentOS'),(15,'Manjaro'),(16,'LFS');
+INSERT INTO `secure` VALUES (1,'Deepin'),(2,'Kali'),(3,'Mint'),(4,'Arch'),(5,'Fedora'),(6,'Parrot'),(7,'Gentoo'),(8,'Tails'),(9,'OpenSUSE'),(10,'Android-x86'),(11,'BackBox'),(12,'Solus'),(13,'DracOs'),(14,'CentOS'),(15,'Manjaro'),(16,'LFS');
 /*!40000 ALTER TABLE `secure` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -319,4 +319,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-06-29 15:42:09
+-- Dump completed on 2019-06-30  2:21:19
